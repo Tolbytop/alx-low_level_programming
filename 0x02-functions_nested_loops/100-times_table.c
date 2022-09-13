0ad631d69547534830ac80025ae2a7129ecebbfb
Your Name <you@example.com>
2022-09-13 11:58:05 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,46 @@
  */
 void print_times_table(int n)
 {
-	int row, column, product;
+		int i, j, k;
 
 	if (n >= 0 && n < 15)
 	{
-		for (row = 0; row <= n; row++)
-		{
-			for (column = 0; column <= n; column++)
+			for (i = 0; i <= n; i++)
 			{
-				product = row * column;
-
-			if (column == 0)
-				_putchar('0');
-			else if (product < 10)
+				for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+				_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
 	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar(product % 10 + '0');
+		_putchar(k + '0');
 	}
-	else if (n > 10 && product <= 100)
+	else if (k >= 10 && k < 100)
 	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar((product / 10) % 10 + '0');
-		_putchar(product % 10 + '0');
+		_putchar((k / 10) + '0');
+		_putchar((k % 10) + '0');
 	}
-	else if (product > 99 && product < 1000)
+				else if (k >= 100)
 	{
 		_putchar(',');
 		_putchar(' ');
-		_putchar(product / 100 + '0');
-		_putchar((product / 10) % 10 + '0');
-		_putchar(product % 10 + '0');
+		_putchar((k / 100) + '0');
+		_putchar(((k / 10) % 10) + '0');
+		_putchar((k % 10) + '0');
 	}
 			}
-			_putchar('\n');
-		}
+				_putchar('\n');
+			}
 	}
 }
 
