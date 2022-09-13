20d4a253371bd89eb5d87ba91fecd9ef71b80025
Your Name <you@example.com>
2022-09-13 09:07:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar(%10 + '0');
+		_putchar(product % 10 + '0');
 	}
 	else if (n > 10 && product <= 100)
 	{
