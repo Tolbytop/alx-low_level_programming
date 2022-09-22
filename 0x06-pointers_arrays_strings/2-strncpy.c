daf60019cd0a8fc9859a664805edd0ef1d98bcab
Your Name <you@example.com>
2022-09-22 00:09:17 -0700

    task 2 corrected

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,19 +8,15 @@
  * @n: number of chars to copy
  * Return: char pointer
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		if (src[i] == '\0')
-		{
-			for (j = i; j < n && dest[j] != '\0'; j++)
-				est[j] = '\0';
-			break;
-		}
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
