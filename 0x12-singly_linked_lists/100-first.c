49dd9ef75f016f2c77dc51580ac0e67b661b3150
Your Name <you@example.com>
2022-10-20 06:57:05 -0700

    code error fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow");
-	rintf(",\nI bore my house upon my back!\n");
+	printf(",\nI bore my house upon my back!\n");
 }
